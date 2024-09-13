9b8c756f68fd3c502bafa2a5d85938150e608a35
Federico.Murru <djfedemurru@icloud.com>
2024-09-13 13:58:43 +0100

    eslint fix

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/HackerRoom";
