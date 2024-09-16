73625e7c34289759cf455ff3c22cde532f729c0a
Federico.Murru <djfedemurru@icloud.com>
2024-09-16 14:02:40 +0100

    .

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -46,7 +46,11 @@ const WorkExperience = () => {
 								>
 									<div className="flex flex-col h-full justify-start items-center py-2">
 										<div className="work-content_logo">
-											<img className="w-full h-full" src={item.icon} alt="" />
+											<img
+												className="w-full h-full"
+												src={item.icon}
+												alt="logo"
+											/>
 										</div>
 
 										<div className="work-content_bar" />
