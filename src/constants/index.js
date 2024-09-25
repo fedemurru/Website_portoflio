67b6bf950fe07049b2f2eaeace3b959c902a3e5d
Federico.Murru <djfedemurru@icloud.com>
2024-09-25 17:40:47 +0100

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -63,27 +63,6 @@ export const clientReviews = [
 
 export const myProjects = [
 	{
-		title: "Podcastr - AI Podcast Platform",
-		desc: "Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.",
-		subdesc:
-			"Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.",
-		href: "https://www.youtube.com/watch?v=zfAb95tJvZQ",
-		href2: "",
-		texture: "/textures/project/project1.mp4",
-
-		spotlight: "/assets/spotlight1.png",
-	},
-	{
-		title: "LiveDoc - Real-Time Google Docs Clone",
-		desc: "LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.",
-		subdesc:
-			"With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.",
-		href: "https://www.youtube.com/watch?v=y5vE8y_f_OM",
-		href2: "",
-		texture: "/textures/project/project2.mp4",
-		spotlight: "/assets/spotlight2.png",
-	},
-	{
 		title: "CarePulse - Health Management System",
 		desc: "An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.",
 		subdesc:
